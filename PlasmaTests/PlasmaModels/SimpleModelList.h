bee79ec1206ed4a5c7ecf378246372f089d4fdda
KAZUMA Ukyo <yaakaito@gmail.com>
2013-01-19 04:44:04 +0900

    added simply model list test.

---
+++ PlasmaTests/PlasmaModels/SimpleModelList.h
@@ -0,0 +1,13 @@
+//
+// Created by yaakaito on 2013/01/19.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+#import "PLAModelList.h"
+
+
+@interface SimpleModelList : PLAModelList
+@end
---
+++ PlasmaTests/PlasmaModels/SimpleModelList.m
@@ -0,0 +1,19 @@
+//
+// Created by yaakaito on 2013/01/19.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "SimpleModelList.h"
+#import "SimpleModel.h"
+
+
+@implementation SimpleModelList {
+
+}
+
++ (Class)listedModelClass {
+    return [SimpleModel class];
+}
+@end
