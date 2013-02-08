630555feb3ac8e09a1632bf236f8bd4f02dad575
KAZUMA Ukyo <yaakaito@gmail.com>
2013-02-09 02:18:33 +0900

    added ResponseParser

---
+++ Plasma/Networking/PLAResponseParser.h
@@ -0,0 +1,13 @@
+//
+// Created by kazuma.ukyo on 2/9/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import <Foundation/Foundation.h>
+
+
+@interface PLAResponseParser : NSValueTransformer
+- (NSDictionary *)parseResponse:(id)response;
+@end
---
+++ Plasma/Networking/PLAResponseParser.m
@@ -0,0 +1,26 @@
+//
+// Created by kazuma.ukyo on 2/9/13.
+//
+// To change the template use AppCode | Preferences | File Templates.
+//
+
+
+#import "PLAResponseParser.h"
+
+
+@implementation PLAResponseParser {
+
+}
+
+- (BOOL)allowsReverseTransformation {
+    return NO;
+}
+
+- (Class)transformedValueClass {
+    return [NSDictionary class];
+}
+
+- (NSDictionary *)parseResponse:(id)response {
+    return (NSDictionary *)response;
+}
+@end
